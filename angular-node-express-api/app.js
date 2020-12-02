fd871959b99a374f728e72bf729bdf682fd82288
CLARVIE <clarvie-edouard.yetikoua@sparna.fr>
2020-12-02 13:06:51 +0100

    Setting dev's environment

---
+++ angular-node-express-api/app.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var users = require('./routes/users');
+var materials = require('./routes/materials');
+var booking = require('./routes/booking');
+
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use('/api/v1/users', users);
+app.use('/api/v1/materials', materials);
+pp.use('/api/v1/booking', materials);
+module.exports = app;
