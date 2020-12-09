f7981290f2652e316acd74eaf50ccd68433ba647
brian-szczepanski <szezedu28@gmail.com>
2020-12-09 17:44:48 +0100

    material.js need console.log

--- backend/controllers/material.js
+++ backend/controllers/material.js
@@ -69,15 +69,19 @@ exports.modifyMaterial = (req, res, next) => {
 
 exports.deleteMaterial = (req, res, next) => {
   Material.deleteOne({_id: req.params.id}).then(
-    () => {
-      res.status(200).json({
-        message: 'Deleted!'
-      });
-    }
+  (toto) => {
+    console.log(toto);
+    res.status(200).json({
+      message: 'Deleted!'
+    });
+  }
   ).catch(
     (error) => {
+
+      //console.log(JSON.stringify(error));
       res.status(400).json({
-        error: error
+        error: error,
+        message: error
       });
     }
   );
