b01dd4c54991de6c4e504120cb30cfb7f6c29de1
CLARVIE <clarvie-edouard.yetikoua@sparna.fr>
2020-12-09 16:41:08 +0100

    Add regex on email

--- backend/models/users.js
+++ backend/models/users.js
@@ -1,13 +1,27 @@
 const mongoose = require('mongoose');
 const uniqueValidator = require('mongoose-unique-validator');
 
+var validateEmail = function(email) {
+    var re = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+    return re.test(email)
+};
+
 const userSchema = mongoose.Schema({
 	nom: { type: String, required: true, unique: false },
 	prenom: { type: String, required: true, unique: false },
-    email: { type: String, required: true, unique: true },
+    email: { 
+    	type: String, 
+    	required: true, 
+    	unique: true,
+    	validate: [validateEmail, 'Invalid email'],
+        match: [/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/, 'Please fill a valid email address'] 
+    },
     role : { type: String, required: true, unique: false },
   	password: { type: String, required: true }
 });
+
+
+
 
 userSchema.plugin(uniqueValidator);
 
